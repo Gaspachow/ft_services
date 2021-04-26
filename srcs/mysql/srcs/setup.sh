3e66185ca26e14718c88fed2ba02c1ab299ac90c
Gaspachow <46502903+Gaspachow@users.noreply.github.com>
2021-04-26 19:46:34 +0200

    small fix: dump delete in mysql setup.sh

--- srcs/mysql/srcs/setup.sh
+++ srcs/mysql/srcs/setup.sh
@@ -2,6 +2,6 @@ mariadb-install-db -u root
 mysqld -u root & sleep 3
 mysql -u root --execute="CREATE DATABASE wordpress;"
 mysql -u root wordpress < /tmp/dump.sql
-rm /tm/dump.sql
+rm /tmp/dump.sql
 mysql -u root --execute="CREATE USER 'root'@'%' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
 telegraf
